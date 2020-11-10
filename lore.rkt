9d0da34a64875a196e20ee4cd56ce61191b1f7d9
srfoster <lindseyhandley@gmail.com>
2020-11-10 14:10:31 -0800

    updated lore

--- lore.rkt
+++ lore.rkt
@@ -9,14 +9,13 @@
 	 preview-image)
 
 (define (name)
-  "Cave World")
+  "Cave")
 
 (define (description)
   @md{
-    This work is a CAVE
+    This work is a Cave.
 
     @(div
-       (alert-warning (b "Note. ")  "We are currently adding new runes and will update this world when we do.")
        (alert-primary "In its current form, we estimate this build to be worth about " (b "3 minutes of fun") ". Explore the nooks and crannies of the cave.  Listen to the sounds.  Discover that you can escape the level and fall into infinity.")
        )
     })
