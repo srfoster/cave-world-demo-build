9c7681b9791b7f8c59294f85c663d46df1e4f970
Stephen Foster <stephen@thoughtstem.com>
2020-11-11 10:11:26 -0800

    fix

--- lore.rkt
+++ lore.rkt
@@ -11,7 +11,6 @@
     This work is a Cave.
 
     @(div
-<<<<<<< HEAD
        (alert-warning (b "Note. ")  "We are currently adding new runes and will update this world when we do.")
        (alert-primary "In its current form, we estimate this build to be worth about " (b "3 minutes of fun") ". Explore the nooks and crannies of the cave.  Listen to the sounds.  Discover that you can escape the level and fall into infinity."))
     }
