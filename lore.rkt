cc3f0a5076ce18f756c556a93bcb50b715378d6c
Stephen Foster <stephen@thoughtstem.com>
2020-11-11 10:05:49 -0800

    new lore

--- lore.rkt
+++ lore.rkt
@@ -1,29 +1,19 @@
 #lang at-exp racket
 
-(require website-js
-	 racket/runtime-path)
+(require codespells/lore)
 
-(provide name
-         description
-         rune-collections
-	 preview-image)
+(define-runtime-path preview-image "images/preview.png")
 
-(define (name)
-  "Cave World")
-
-(define (description)
+(define-authored-work-lore
+  #:name "Cave" 
+  #:description 
   @md{
     This work is a CAVE
 
     @(div
        (alert-warning (b "Note. ")  "We are currently adding new runes and will update this world when we do.")
-       (alert-primary "In its current form, we estimate this build to be worth about " (b "3 minutes of fun") ". Explore the nooks and crannies of the cave.  Listen to the sounds.  Discover that you can escape the level and fall into infinity.")
-       )
-    })
-
-(define (rune-collections)
-  (list
-    'fire-particles))
-
-(define-runtime-path preview-image "images/preview.png")
-
+       (alert-primary "In its current form, we estimate this build to be worth about " (b "3 minutes of fun") ". Explore the nooks and crannies of the cave.  Listen to the sounds.  Discover that you can escape the level and fall into infinity."))
+    }
+  #:rune-collections
+  (list 'fire-particles)
+  #:preview-image preview-image)
