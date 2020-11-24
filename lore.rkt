c6a111cc46046f53f6ef3342ee6294455fd4379e
srfoster <lindseyhandley@gmail.com>
2020-11-24 14:47:53 -0800

    fixed lore to explain limited world

--- lore.rkt
+++ lore.rkt
@@ -6,7 +6,7 @@
   #:name "Cave" 
   #:description 
   @md{
-    The @b{Cave of Shorengard the Great} holds many mysteries. It is said that this is where @b{Kyri the Enchantress} drew her last breath over ten thousand years ago. But that is a tale for another time...
+    Built during the @b{First Epoch} when wizards could not yet build infinite worlds, the @b{Cave of Shorengard the Great} holds many mysteries. It is said that this is where @b{Kyri the Enchantress} drew her last breath over ten thousand years ago. But that is a tale for another time...
 
     @(div
        (alert-primary "In its current form, we estimate this build to be worth about " (b "3 minutes of fun") ". Explore the nooks and crannies of the cave.  Listen to the sounds.  Discover that you can escape the level and fall into infinity."))
